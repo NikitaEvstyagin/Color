3301bab46c861e81fa019c46191318d931392854
nikita2005553 <evstyagin09@gmail.com>
2022-11-29 11:06:30 +0300

    test

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp2 {
+﻿using System.Drawing;
+
+namespace ConsoleApp2 {
     public class Program
     {
         public static void Main() {
@@ -6,7 +8,19 @@
             ColorRGB colorRGB2 = ColorRGB.Color_CMYM(5, 0, -2);
             ColorCMY colorCMY1 = ColorCMY.Color_CMYM(1, 2, 3);
             ColorCMY colorCMY2 = ColorCMY.Color_CMYM(2, 3, -26);
+
             Console.WriteLine(colorRGB1);
+            Console.WriteLine(colorRGB2);
+            Console.WriteLine();
+            Console.WriteLine(colorRGB1 + colorRGB2);
+            Console.WriteLine(colorRGB1 - colorRGB2);
+            Console.WriteLine();
+            Console.WriteLine(colorCMY1);
+            Console.WriteLine(colorCMY2);
+            Console.WriteLine();
+            Console.WriteLine(colorCMY1 + colorCMY2);
+            Console.WriteLine(colorCMY1 - colorCMY2);
+        
         }
     }
 }
